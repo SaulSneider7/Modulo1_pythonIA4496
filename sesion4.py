6588cf2381816b5fe180fa3c193e55958da5efa8
saulsneider7 <saulsneider@hotmail.com>
2024-02-20 16:28:54 -0500

    sesion 4 actividad 2

--- sesion4.py
+++ sesion4.py
@@ -26,6 +26,24 @@ while True:
     #Mostrar la imagen en pantalla
     cv2.imshow('Frame', frame)
 
+    # Actividad 2: Usar filtros usando la camara
+    contraste = 1.2     # Mantenerlo en el rango de 0.0 a 2.0
+    brillo = -20        # Mantenerlo en el rango -100 a 100
+    nueva = cv2.convertScaleAbs(frame, alpha=contraste, beta=brillo)
+
+    # Mejorar enfoque
+    kernel = -1 * np.ones((5, 5), np.float32)
+    kernel[2, 2] = 25
+    enfoque = cv2.filter2D(nueva, -1, kernel)
+    cv2.imshow('Enfoque', enfoque)
+
+    #--------------------------------------------------------------
+    # Crear caricatura de la foto
+
+    # Convertir imagen a grises
+    grises = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
 
 
 # -------------------------------------------------------------------------
