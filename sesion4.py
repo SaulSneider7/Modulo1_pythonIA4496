73581179c208977273f2dc7e5be90deea287d832
saulsneider7 <saulsneider@hotmail.com>
2024-02-20 16:12:36 -0500

    sesion 4

--- sesion4.py
+++ sesion4.py
@@ -10,8 +10,21 @@ import numpy as np
 
 # -------------------------------------------------------------------------
 # Escribe tu código aquí:
+# Actividad 1: Utilizar camara web
 
+# Abrir camara
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
+# Utiliza la camara hasta que la tecla "q" sea presionada
+while True:
+    ret, frame = cap.read()
+
+    #comprobar que exista una imagen
+    if not ret:
+        break
+
+    #Mostrar la imagen en pantalla
+    cv2.imshow('Frame', frame)
 
 
 
